fdb8798675eac6364c32a872361e10fd10f1efd6
WilliamsRv <Williams.rosas.v@vallegrande.edu.pe>
2024-03-03 21:35:35 -0500

    sitio web

---
+++ home.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper",{
+    slidesPerview: 1,
+    spaceBetween: 100,
+    grabCursor:true,
+    lopp:true,
+    breackpoints:{
+        991: {
+            slidesPerview:1
+        }
+    }
+})
